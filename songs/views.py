70d817d7a3dd598b5ea278572d11430c52f8fa2b
Renato <rcoelhomf@gmail.com>
2023-11-29 19:58:53 -0300

    project clone

---
+++ songs/views.py
@@ -0,0 +1,16 @@
+from rest_framework.generics import ListCreateAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Song
+from .serializers import SongSerializer
+
+
+class SongView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(album_id=self.kwargs['pk'])
---
+++ users/views.py
@@ -0,0 +1,18 @@
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import UserSerializer
+from .permissions import IsAccountOwner
+
+
+class UserView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
